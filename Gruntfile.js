76030df3bcae425e4dac9f9105dbc628d271acfd
willerce <willerce@gmail.com>
2014-02-22 18:46:29 +0800

    add mac icon

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,8 @@ module.exports = function(grunt) {
   grunt.initConfig({
     nodewebkit: {
       options: {
-        build_dir: './builds', 
+        build_dir: './builds',
+        mac_icns: './src/resource/icon512x512.icns',
         mac: true, 
         win: true, 
         linux32: false, 
